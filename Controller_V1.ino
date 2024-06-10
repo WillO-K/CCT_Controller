939c87aef008c9bb0b96f3d28697380a1d3a6c9f
WillO-K <122154268+WillO-K@users.noreply.github.com>
2024-06-10 01:10:47 +0100

    cleaning up readme and making variables easier to read

--- Controller_V1.ino
+++ Controller_V1.ino
@@ -1,77 +1,64 @@
 // Authored by Willoughby Kemble
-// Not to be used commercially.
-// This is just a really basic test program for my custom PCB to control dual-white LED strips. The board uses an ATMEGA8A-AU to control everything, with a stepdown converter on the board, stepping down the input
-// of 12V to 5V so it's acceptable for the ATMEGA, but then providing 12V to the LED's VIN directly. 
-// We also have 3 tactile buttons mounted to the board too. We're using MOSFETS for the output of 3 PWM pins on the board to control the warm white and cold white channels on the board.
-// You can very easily replicate this setup with an Arduino UNO, Nano or Pro-mini or something similar, 
-// as long as you have access to 3 PWM pins, as CCT strips are usually 'dumb' strips and can't use signals like WS2812B (or any WS2xxx series strips/leds) strips can.
+// Designed specifically for a custom PCB using an ATMEGA8A-AU, but can be used on any Arduino-based device or board using ATMEGA chips similar to the 8A
 
-// This is only a test program, I do plan to rework the PCB to make it more elegant, and the code a bit more robust, with more features (as the componants on the board may also change during refinement)
-// This is only intended for prototyping.
 
-int pwm1 = 9;
-int pwm2 = 10;
-int buttonPin1 = 2;
-int buttonPin2 = 3;
-int buttonPin3 = 4;
+int WarmWhiteOutput = 9;
+int ColdWhiteOutput = 10;
+int WarmWhiteIncreaseButton = 2;
+int ColdWhiteIncreaseButton = 3;
+int MaxBrightnessButton = 4;
 
-int brightness1 = 0; // initial brightness value for pwm1 (pwm1 could be warm white or cold white, depends which way you plug them in on the board)
-int brightness2 = 255; // initial brightness value for pwm2 (same applies for pwm2)
+int WarmWhiteBrightness = 0; 
+int ColdWhiteBrightness = 255; 
 
 void setup() {
-    pinMode(pwm1, OUTPUT);
-    pinMode(pwm2, OUTPUT);
-    pinMode(buttonPin1, INPUT_PULLUP); // Using an external pullup resistor of 10K for each button
-    pinMode(buttonPin2, INPUT_PULLUP);
+    pinMode(WarmWhiteOutput, OUTPUT);
+    pinMode(ColdWhiteOutput, OUTPUT);
+    pinMode(WarmWhiteIncreaseButton, INPUT_PULLUP);
+    pinMode(ColdWhiteIncreaseButton, INPUT_PULLUP);
 
-    analogWrite(pwm1, brightness1);
-    analogWrite(pwm2, brightness2);
+    analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
+    analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
 
     Serial.begin(9600);
 }
 
 void loop() {
-    // Check if button1 is being pressed, if so then we increase the brightness of 1 channel (being coldwhite or warmwhite depending on which way they're wired), and decrease it for the other
-    if (digitalRead(buttonPin1) == LOW) {
-        brightness1 += 25; // Increase brightness1 by 25
-        brightness2 -= 25; // Decrease brightness2 by 25
+    if (digitalRead(WarmWhiteIncreaseButton) == LOW) {
+        WarmWhiteBrightness += 25;
+        ColdWhiteBrightness -= 25;
         
-        // We want to constrain the brightness to 255, which is typical for LEDs. 
-        brightness1 = constrain(brightness1, 0, 255);
-        brightness2 = constrain(brightness2, 0, 255);
+        WarmWhiteBrightness = constrain(WarmWhiteBrightness, 0, 255);
+        ColdWhiteBrightness = constrain(ColdWhiteBrightness, 0, 255);
         
-        // Set the new brightness values
-        analogWrite(pwm1, brightness1);
-        analogWrite(pwm2, brightness2);
-        
-        // Little delay to debounce the button press
-        delay(200);
-    }
-
-    // Check if button2 is being pressed, if so then we do the reverse of what occurs in the first segment of the loop.
-    if (digitalRead(buttonPin2) == LOW) {
-        brightness1 -= 25; // Decrease brightness1 by 25
-        brightness2 += 25; // Increase brightness2 by 25
-        
-        brightness1 = constrain(brightness1, 0, 255);
-        brightness2 = constrain(brightness2, 0, 255);
-        
-        analogWrite(pwm1, brightness1);
-        analogWrite(pwm2, brightness2);
+        analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
+        analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
         
         delay(200);
     }
-    // This ones a little different, if we want to set the strip to have both warm white and cold white channels at full brightness, then button3 needs to be pressed.
-    // Having both at full brightness does create a different shade of white, and is much brighter. 
-    if (digitalRead(buttonPin3) == LOW) {
-      brightness1 +=255;
-      brightness2 +=255;
 
-        brightness1 = constrain(brightness1, 0, 255);
-        brightness2 = constrain(brightness2, 0, 255);
+    if (digitalRead(ColdWhiteIncreaseButton) == LOW) {
+        WarmWhiteBrightness -= 25; 
+        ColdWhiteBrightness += 25; 
         
-        analogWrite(pwm1, brightness1);
-        analogWrite(pwm2, brightness2);
+        WarmWhiteBrightness = constrain(WarmWhiteBrightness, 0, 255);
+        ColdWhiteBrightness = constrain(ColdWhiteBrightness, 0, 255);
+        
+        analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
+        analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
+        
+        delay(200);
+    }
+
+    if (digitalRead(MaxBrightnessButton) == LOW) {
+      WarmWhiteBrightness +=255;
+      ColdWhiteBrightness +=255;
+
+        WarmWhiteBrightness = constrain(WarmWhiteBrightness, 0, 255);
+        ColdWhiteBrightness = constrain(ColdWhiteBrightness, 0, 255);
+        
+        analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
+        analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
         
         delay(200);
     }
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # CCT_Controller
-Software to upload to an Arduino-capable chip to control CCT (Dual white) LED strips.
-Feel free to use this code for **non-commercial** personal projects.
+## What does it do?
 
-The goal of this project is to create a custom CCT (dual-white) LED controller. The board being used is a custom PCB I have designed, but can be easily replicated on any Arduino with some additional componants. 
+This program allows you to adjust the warm white and cold white channels on a CCT (dual white) 'dumb' LED strip. 
+
+## Board setup
+
+The code was written with the intention of being used on a custom PCB using an ATMEGA8A-AU, but can be used on any Arduino board, so long as you have 2 PWM pins you can use for the white channels. You'll need 3 buttons; these control the brightness of the each channel, as well as one button setting the brightness of both channels to the maximum, and (more than likely), a method of stepping your voltage down to whatever is suitable for your board, as CCT strips usually consume 12V-24V. 
+
+If you're building a board or using a microcontroller on it's own (for example, the popular ATMEGA328-PU), you should consider the following:
+- MOSFETS to control the signal to the LED strip.
+- 10K resistors as pullup resistors for the buttons.
+- A step-down converter, most CCT strips are 12V, the microcontroller can't handle this, so you'll need a step-down converter to step it down to 5V (or whatever working voltage your particular MC uses, some are 3.3V I think).
+- Decoupling capacitors - to remove noise and keep things stable. 
+
+## How to use
+
+Once your board is setup, open the .ino file in Arduino IDE, select your board, and hit the "Upload" button. 
