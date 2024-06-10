a3d72ede83fff878f059c03d0ab9b073ba2b517f
WillO-K <122154268+WillO-K@users.noreply.github.com>
2024-06-10 01:38:46 +0100

    refactoring

--- Controller_V1.ino
+++ Controller_V1.ino
@@ -2,14 +2,20 @@
 // Designed specifically for a custom PCB using an ATMEGA8A-AU, but can be used on any Arduino-based device or board using ATMEGA chips similar to the 8A
 
 
-int WarmWhiteOutput = 9;
-int ColdWhiteOutput = 10;
-int WarmWhiteIncreaseButton = 2;
-int ColdWhiteIncreaseButton = 3;
-int MaxBrightnessButton = 4;
+const int WarmWhiteOutput = 9;
+const int ColdWhiteOutput = 10;
+const int WarmWhiteIncreaseButton = 2;
+const int ColdWhiteIncreaseButton = 3;
+const int MaxBrightnessButton = 4;
+
+const int MaxBrightness = 255;
 
 int WarmWhiteBrightness = 0; 
 int ColdWhiteBrightness = 255; 
+
+const int ButtonValue = ButtonValue;
+
+const int debounceVal = 200;
 
 void setup() {
     pinMode(WarmWhiteOutput, OUTPUT);
@@ -23,43 +29,47 @@ void setup() {
     Serial.begin(9600);
 }
 
+int constrainBrightness(int brightness) {
+    return constrain(brightness, 0, 255);
+}
+
 void loop() {
     if (digitalRead(WarmWhiteIncreaseButton) == LOW) {
-        WarmWhiteBrightness += 25;
-        ColdWhiteBrightness -= 25;
+        WarmWhiteBrightness += ButtonValue;
+        ColdWhiteBrightness -= ButtonValue;
         
-        WarmWhiteBrightness = constrain(WarmWhiteBrightness, 0, 255);
-        ColdWhiteBrightness = constrain(ColdWhiteBrightness, 0, 255);
+        WarmWhiteBrightness = constrainBrightness(WarmWhiteBrightness);
+        ColdWhiteBrightness = constrainBrightness(ColdWhiteBrightness);
         
         analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
         analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
         
-        delay(200);
+        delay(debounceVal);
     }
 
     if (digitalRead(ColdWhiteIncreaseButton) == LOW) {
-        WarmWhiteBrightness -= 25; 
-        ColdWhiteBrightness += 25; 
+        WarmWhiteBrightness -= ButtonValue; 
+        ColdWhiteBrightness += ButtonValue; 
         
-        WarmWhiteBrightness = constrain(WarmWhiteBrightness, 0, 255);
-        ColdWhiteBrightness = constrain(ColdWhiteBrightness, 0, 255);
+        WarmWhiteBrightness = constrainBrightness(WarmWhiteBrightness);
+        ColdWhiteBrightness = constrainBrightness(ColdWhiteBrightness);
         
         analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
         analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
         
-        delay(200);
+        delay(debounceVal);
     }
 
     if (digitalRead(MaxBrightnessButton) == LOW) {
-      WarmWhiteBrightness +=255;
-      ColdWhiteBrightness +=255;
+      WarmWhiteBrightness +=MaxBrightness;
+      ColdWhiteBrightness +=MaxBrightness;
 
-        WarmWhiteBrightness = constrain(WarmWhiteBrightness, 0, 255);
-        ColdWhiteBrightness = constrain(ColdWhiteBrightness, 0, 255);
+        WarmWhiteBrightness = constrainBrightness(WarmWhiteBrightness);
+        ColdWhiteBrightness = constrainBrightness(ColdWhiteBrightness);
         
         analogWrite(WarmWhiteOutput, WarmWhiteBrightness);
         analogWrite(ColdWhiteOutput, ColdWhiteBrightness);
         
-        delay(200);
+        delay(debounceVal);
     }
 }
